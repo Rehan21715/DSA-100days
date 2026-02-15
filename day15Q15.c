e85ab928c56be356776debdabe08373032e87769
Rehan  Mahboob <rehanmahboob29@gmail.com>
2026-02-15 19:28:29 +0530

    Add files via upload

---
+++ day15Q15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    int sum = 0;
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for(int i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+    printf("%d\n", sum);
+
+    return 0;
+}
