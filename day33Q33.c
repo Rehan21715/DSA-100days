7c0599f357a667238b314e8a9d6134f6405f8810
Rehan  Mahboob <rehanmahboob29@gmail.com>
2026-03-05 20:45:30 +0530

    Add files via upload

---
+++ day33Q33.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+char stack[100];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int priority(char x)
+{
+    if(x == '(') return 0;
+    if(x == '+' || x == '-') return 1;
+    if(x == '*' || x == '/') return 2;
+    return 3;
+}
+
+int main()
+{
+    char exp[100];
+    scanf("%s", exp);
+    
+    for(int i = 0; i < strlen(exp); i++)
+    {
+        if(isalnum(exp[i]))
+            printf("%c", exp[i]);
+        else if(exp[i] == '(')
+            push(exp[i]);
+        else if(exp[i] == ')')
+        {
+            while(stack[top] != '(')
+                printf("%c", pop());
+            pop();
+        }
+        else
+        {
+            while(priority(stack[top]) >= priority(exp[i]))
+                printf("%c", pop());
+            push(exp[i]);
+        }
+    }
+
+    while(top != -1)
+        printf("%c", pop());
+
+    return 0;
+}
